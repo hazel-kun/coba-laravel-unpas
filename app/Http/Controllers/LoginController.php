85e87dc1b0bbe8b530d6ec7e15886eec39921f24
Muhammad Wildanul Falah <wildan.gant12@gmail.com>
2024-05-17 07:53:32 +0700

    .

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -27,7 +27,7 @@ class LoginController extends Controller
             return redirect()->intended('/dashboard');
         }
 
-        return back()->with('loginError', 'Login failed!');
+        return back()->with('loginError', 'Email atau password anda salah!');
     }
 
     public function logout()
